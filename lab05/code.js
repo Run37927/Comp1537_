cbcb3d10e2e7c2d0046650f5916e12a61486c3be
Hairun Huang <hairunhuang@gmail.com>
2022-02-08 17:11:05 -0800

    lab5

---
+++ lab05/code.js
@@ -0,0 +1,12 @@
+function calculate_area() {
+    r = parseInt(jQuery("#x").val());
+
+    jQuery("#p1").html(r * r * Math.PI)
+}
+
+
+function setup() {
+    console.log("setup() got called")
+    jQuery("#calc").click(calculate_area);
+}
+jQuery(document).ready(setup);
