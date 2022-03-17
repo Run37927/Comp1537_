2e5a29b96a6dd0736c6ebc5638e9561cfb8aaaab
Hairun Huang <hairunhuang@gmail.com>
2022-03-16 19:49:26 -0700

    2

--- assign2/movie.js
+++ assign2/movie.js
@@ -1,29 +1,3 @@
-function chuma(data) {
-
-    for(i = 0; i < data.results.length; i++) {
-        // console.log(data.results[i].original_title + "<br>");
-        // console.log(data.results[i].overview);
-        // console.log(data.results[i].poster_path);
-
-      
-        $('#para').append(data.results[i].original_title + "<br>");
-        $('#para').append(data.results[i].overview +"<br>");
-     
-        address = data.results[i].poster_path
-        img = `<img src='https://image.tmdb.org/t/p/w500/${address}'>`
-
-        $('#para').append(img +"<br>");
-
-        img = data.results[i].backdrop_path
-        img_drop = `<button id="${img}" class="back_drop">Display BackDrop Image</button>`
-        $('#para').append( img_drop + "<br>");
-        
-    
-    }  
-
-}
-
-
 function displayBackDrop() {
     x = $(this).attr('id')
     $("#backdrop").html(`<img src="https://image.tmdb.org/t/p/original${x}" width="100%">`)    
@@ -31,13 +5,28 @@ function displayBackDrop() {
 
 
 function getInfo() {
+  
     x = $("#movie_name").val();
     $.ajax(
         {
-           
             "url":`https://api.themoviedb.org/3/search/movie?api_key=ed4ef9b0f9bcb9c237ab83a2c2ffb909&query=${x}`,
             "type": "GET",
-            "success": chuma
+            "success": function chuma(data) {
+                for(i = 0; i < data.results.length; i++) {
+               
+                    $('#result').append(data.results[i].original_title + "<br>");
+                    $('#result').append(data.results[i].overview +"<br>");
+                 
+                    address = data.results[i].poster_path
+                    img = `<img src='https://image.tmdb.org/t/p/w500/${address}' height="100px">`
+            
+                    $('#result').append(img +"<br>");
+            
+                    backdrop = data.results[i].backdrop_path
+                    img_drop = `<button id="${backdrop}" class="back_drop">Display BackDrop Image</button>`
+                    $('#result').append( img_drop + "<hr>");
+                }  
+            }
         }
     )
 }
@@ -46,6 +35,7 @@ function getInfo() {
 function setup() {
     $('#getMovie').click(getInfo);
     $('body').on("click", ".back_drop", displayBackDrop)
+    
 
 }
 
