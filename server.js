26fa5545b9676c5a7588ef43c8e1877cc6188954
Hairun Huang <hairunhuang@gmail.com>
2022-05-13 21:06:57 -0700

    replace with my own api

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs');
-const https = require('https')
+const http = require('http');
+const https = require('https');
+const pokemons = require("./pokemon.json")
+const abilities = require("./ability.json")
+const types = require("./type.json")
+const serverUrl = "http://localhost:5000"
+const mongoose = require('mongoose');
 
 app.listen(process.env.PORT || 5000, function (err) {
     if (err)
@@ -10,12 +16,35 @@ app.listen(process.env.PORT || 5000, function (err) {
 
 app.use(express.static('./public'));
 
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}));
+
+
+app.get("/api/v2/pokemon/:pokemonid", function(req, res){
+    res.send(pokemons.pokemon[req.params.pokemonid])
+})
+
+app.get("/api/v2/pokemon/:pokemonname", function(req, res){
+    res.send(pokemons.pokemon[req.params.pokemonname])
+})
+
+app.get("/api/v2/ability/:pokeability", function(req, res){
+    res.send(abilities.pokemon[req.params.pokeability])
+})
+
+
 app.get('/profile/:id', function (req, res){
-    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+    // const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+    const url = `${serverUrl}/api/v2/pokemon/${req.params.id}`
+
 
     data = ""
-
-    https.get(url, function(https_res) {
+    http.get(url, function(https_res) {
         https_res.on("data", function(chunk) {
             data += chunk;
         })
@@ -79,7 +108,76 @@ app.get('/profile/:id', function (req, res){
  
 })
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + "/index.html");
-// })
 
+mongoose.connect("mongodb://localhost:27017/timelineDB",
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const eventSchema = new mongoose.Schema({
+  text: String,
+  hits: Number,
+  time: String
+});
+
+const eventModel = mongoose.model("timelineevents", eventSchema);
+
+// the read in CRUD
+app.get('/timeline/getAllEvents', function(req,res) {
+    eventModel.find({ }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('data' + data);
+        }
+
+        res.send(data);
+    })
+})
+
+// THE CREATE IN CRUD
+app.put('/timeline/insert', function(req,res) {
+    console.log(req.body)
+    eventModel.create({
+        text: req.body.text,
+        time: req.body.time,
+        hits: req.body.hits
+    }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('data' + data);
+        }
+
+        res.send(data);
+    })
+})
+
+// THE update IN CRUD
+app.get('/timeline/increaseHits/:id', function(req,res) {
+    console.log(req.params)
+    eventModel.updateOne({
+        _id: req.params.id
+    }, { $inc: { hits: 1} }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('data' + data);
+        }
+
+        res.send("update is successful");
+    })
+})
+
+
+// THE delete IN CRUD
+app.get('/timeline/remove/:id', function(req,res) {
+    
+    eventModel.remove({ _id: req.params.id }, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('data' + data);
+        }
+
+        res.send("delete is successful");
+    })
+})
